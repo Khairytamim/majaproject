b78daca8a18e7a9a9ff7ed1edfc6f8907e580e1d
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2016-05-21 18:45:05 +0700

    nambahin database maja

---
+++ database/migrations/2016_05_21_110750_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id_product');
+            $table->string('product_name');
+            $table->string('brand');
+            $table->text('descriptions');
+            $table->integer('category_id')->unsigned()->index();
+            $table->foreign('category_id')->references('id_category')->on('category')->onDelete('cascade');
+            $table->string('gender');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('product');
+    }
+}

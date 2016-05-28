68e76729bfc282fdcb1da408388fb45cc3ca2620
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2016-05-28 19:09:52 +0700

    update tabel products

--- database/migrations/2016_05_28_063909_create_products_table.php
+++ database/migrations/2016_05_28_063909_create_products_table.php
@@ -14,12 +14,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id_product');
-            $table->integer('id_user')->usignet();
+            $table->integer('id_user')->unsigned();
             $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
 
             $table->string('product_name');
             $table->string('gender');
-            $table->integer('category_id');
+            $table->integer('category_id')->unsigned();
             $table->foreign('category_id')->references('id_category')->on('category')->onDelete('cascade');
             
             $table->text('descriptions');

087338ba1c829ebbc2aca991ffb989d9970735f3
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2016-05-28 18:47:40 +0700

    update tabel products

--- app/User.php
+++ app/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name', 'username', 'email', 'password',
-    ];
+    ]; 
 
     /**
      * The attributes that should be hidden for arrays.

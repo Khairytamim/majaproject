1a93fd26811e1f777502651903ef947018759548
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2016-05-28 20:48:34 +0700

    update tabel shop

---
+++ database/migrations/2016_05_28_133645_create_shop_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShopTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shop', function (Blueprint $table) {
+            $table->increments('shop_id');
+            $table->string('shop');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('shop');
+    }
+}

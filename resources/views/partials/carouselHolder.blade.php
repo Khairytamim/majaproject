30732eeea1af5c60868a93a429030e9c44b6a6a4
Mustika Kurnia M <nia.kurniam@yahoo.co.id>
2016-05-29 18:24:11 +0700

    update slider

--- resources/views/partials/carouselHolder.blade.php
+++ resources/views/partials/carouselHolder.blade.php
@@ -7,25 +7,14 @@
                 <ol class="carousel-indicators">
                     <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
                     <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
                 </ol>
                 <div class="carousel-inner">
                     <div class="item active">
-                        <img class="slide-image" src="/images/6831581-book.jpg" alt="">
-                        <div class="container">
-                            <div class="carousel-caption">
-                                <h1>Example headline.</h1>
-                                <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-                                <p><a role="button" href="#" class="btn btn-primary btn-xs">Sign up today</a></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img class="slide-image" src="/images/maja_slider.jpg" alt="">
+                        <img class="slide-image" src="/images/maja_slider.jpg" alt="maja">
                     </div>
                     <div class="item">
                         {{--  <img class="slide-image" src="https://placeholdit.imgix.net/~text?txtsize=75&txt=800%C3%97300&w=850&h=220" alt="">   --}}
-                        <img class="slide-image" src="/images/mood-book.jpg" alt="">
+                        <img class="slide-image" src="/images/slider2.jpg" alt="maja2">
                     </div>
                 </div>
                 <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
